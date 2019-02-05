613cd50f2149155e0536f2c4bfb923fc47284ca7
John Gaines <Firefly4266@users.noreply.github.com>
2019-02-04 23:53:05 -0800

    Added three pages

---
+++ about-me/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = ()=>{
+    return(
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In sint corrupti quidem nisi fuga eveniet vitae iure maxime assumenda labore? Dolor repudiandae fugiat illum in incidunt ipsam eos voluptates consequuntur!</p>
+        </div>
+    )
+}
+
+export default About
---
+++ about-me/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = ()=>{
+    return(
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In sint corrupti quidem nisi fuga eveniet vitae iure maxime assumenda labore? Dolor repudiandae fugiat illum in incidunt ipsam eos voluptates consequuntur!</p>
+        </div>
+    )
+}
+
+export default Home
