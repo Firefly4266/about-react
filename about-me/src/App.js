c9b8c210fd6a62ca4f4eaeb46d21230e804cea82
Firefly4266 <gaines.johnp@gmail.com>
2019-02-09 19:42:35 -0800

    adding router files

--- about-me/src/App.js
+++ about-me/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
           <Route exact path='/' component={ Home } />
           <Route path='/about' component={ About } />
           <Route path='/contact' component={ Contact } />
-          </div>
+        </div>
       </BrowserRouter>
     );
   }
